2ad26981a6f2ba790fc059c0ff5f304ccff5c548
granitnebiu <granitnebiu@gmail.com>
2023-03-09 22:37:04 +0100

    blog page coming soon

---
+++ pages/blog.tsx
@@ -0,0 +1,33 @@
+import Button from "@/partials/Button";
+import Title from "@/partials/Title";
+import DefaultLayout from "Layout/DefaultLayout";
+import Image from "next/image";
+import React from "react";
+
+export default function Blog() {
+   return (
+      <DefaultLayout>
+         <section className="relative ">
+            <Image
+               src="/images/about-us.jpg"
+               alt="background case studies"
+               width="0"
+               height="0"
+               sizes="100vw"
+               className="pointer-events-none absolute top-0 -z-10 h-full w-full object-cover"
+            />
+            <div className="bg-secondary/30">
+               <section className="section_container flex h-60 items-center ">
+                  <Title
+                     title="Contact Us"
+                     titleStyle="text-[60px] text-white"
+                  />
+               </section>
+            </div>
+         </section>
+         <section className="section_container flex h-60 items-center">
+            Coming Soon
+         </section>
+      </DefaultLayout>
+   );
+}
